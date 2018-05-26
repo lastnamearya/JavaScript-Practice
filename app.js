06dd31d08a88c7197b6e909ee89d73d831613691
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-26 12:12:50 +0530

    this keyword in methods of classes

---
+++ app.js
@@ -0,0 +1,15 @@
+function Donuts(color, size) {
+  this.color = color;
+  this.size = size;
+  // By default it is set to order === 10
+  this.order = 10;
+
+  // We can also define methods in Classes
+  this.rate = function() {
+    return 'I order 10 donuts and size is ' + this.size;
+  }
+}
+
+var veganDonuts = new Donuts('pink', 'L');
+
+console.log(veganDonuts.rate())
