23e7ac9eda9794967a42a1179a95de120077c2e0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-12 22:15:14 +0530

    window binding with 'this' keyword

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-var sayName = function(lang1, lang2, lang3) {
-  console.log(
-    `Hi my Name is ${this.name} and I know ${lang1}, ${lang2} and ${lang3}`
-  );
+// window binding
+
+const age = 25;
+
+var sayAge = function() {
+  "use strict";
+  console.log(this.age);
 };
 
-var stacey = {
-  name: "Jigyasu",
-  age: 25
-};
+// It'll throw an error to us ~ window binding with 'strict mode'
 
-var languages = ["JavaScript", "Ruby", "Python"];
-
-// During .call() this will not binds to the global window like regular function execution
-
-// .apply() is similiar like .call() property but what it does is, it'll automatically going to parse the array for the arguments. We don't have to explicity pass the array index values for function execution.
-
-// .bind() is similar like .call() but rather then executing the function, it'll give us a new function which which's reference we can store somewhere.
-
-var newFn = sayName.bind(stacey, languages[0], languages[1], languages[2]);
-
-newFn();
+sayAge();
