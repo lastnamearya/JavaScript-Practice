a943550474c526e0af57680d9336c3107e1d0288
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-12 21:59:59 +0530

    .call() function execution this

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var sayName = function() {
-  console.log("Hi My name is " + this.name);
+var sayName = function(lang1, lang2, lang3) {
+  console.log(
+    `Hi my Name is ${this.name} and I know ${lang1}, ${lang2} and ${lang3}`
+  );
 };
 
-var name = "Jigyasu";
+var stacey = {
+  name: "Jigyasu",
+  age: 25
+};
 
-sayName.call();
+var languages = ["JavaScript", "Ruby", "Python"];
+
+// During .call() this will not binds to the global window like regular function execution
+
+sayName.call(stacey, languages[0], languages[1], languages[2]);
