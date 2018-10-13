ee673cb777825875b7b833a623fb7b29b8cc479a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-13 16:28:41 +0530

    Pseudoclassical Pattern instance of

--- app.js
+++ app.js
@@ -1,48 +1,10 @@
-class Animal {
-  constructor(name, energy) {
-    this.name = name;
-    this.energy = energy;
+function Animal(name, energy) {
+  if (this instanceof Animal === false) {
+    console.log("Forget to call Animal with the new keyword");
   }
 
-  eat(amount) {
-    console.log(`${this.name} is eating.`);
-    this.energy += amount;
-  }
-
-  sleep(length) {
-    console.log(`${this.name} is sleeping.`);
-    this.energy += length;
-  }
-
-  play(length) {
-    console.log(`${this.name} is playing.`);
-    this.energy -= length;
-  }
-
-  // Approach 2: Built in Solution by Js ~ static keyword
-
-  static nextToEat(animals) {
-    const sortedByLeastEnergy = animals.sort((a, b) => {
-      return a.energy - b.energy;
-    });
-
-    return sortedByLeastEnergy[0].name;
-  }
+  this.name = name;
+  this.energy = energy;
 }
 
-// // Approach 1
-
-// // Static Method that we don't want to have in the class but don't want to share with the instances ~ Because it's like a helper function.
-
-// function nextToEat(animals) {
-//   const sortedByLeastEnergy = animals.sort((a, b) => {
-//     return a.energy - b.energy;
-//   });
-
-//   return sortedByLeastEnergy[0].name;
-// }
-
-const leo = new Animal("Leo", 7);
-const snoop = new Animal("Snoop", 10);
-
-console.log(Animal.nextToEat([leo, snoop]));
+const leo = Animal("Leo", 7);
