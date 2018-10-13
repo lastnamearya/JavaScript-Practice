fde9b056f9357a87e7d738c7d7f3b69fbefa8b2d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-13 15:16:19 +0530

    helper function with class

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-// Arrow function can't be a constructor function, because they don't have this keyword or "this" won't pass by default in the arguments
+class Animal {
+  constructor(name, energy) {
+    this.name = name;
+    this.energy = energy;
+  }
 
-const Animal = () => {};
+  eat(amount) {
+    console.log(`${this.name} is eating.`);
+    this.energy += amount;
+  }
 
-const leo = new Animal();
+  sleep(length) {
+    console.log(`${this.name} is sleeping.`);
+    this.energy += length;
+  }
+
+  play(length) {
+    console.log(`${this.name} is playing.`);
+    this.energy -= length;
+  }
+}
+
+// Static Method that we don't want to have in the class but don't want to share with the instances ~ Because it's like a helper function.
+
+function nextToEat(animals) {
+  const sortedByLeastEnergy = animals.sort((a, b) => {
+    return a.energy - b.energy;
+  });
+
+  return sortedByLeastEnergy[0].name;
+}
+
+const leo = new Animal("Leo", 7);
+const snoop = new Animal("Snoop", 10);
+
+console.log(nextToEat([leo, snoop]));
