223a4ff7c31a8031b691e776d57705451f8255ba
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-26 00:00:17 +0530

    callbacks converted into a promise

--- app.js
+++ app.js
@@ -12,37 +12,40 @@ function getLocationURL([city, state]) {
 
 const id = "lastnamearya";
 
-function getUser(id, onSuccess, onFailure) {
-  $.getJSON({
-    url: `https://api.github.com/users/${id}`,
-    success: onSuccess,
-    error: onFailure
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    $.getJSON({
+      url: `https://api.github.com/users/${id}`,
+      success: resolve,
+      error: reject
+    });
   });
 }
 
-function getWeather(user, onSuccess, onFailure) {
-  $.getJSON({
-    url: getLocationURL(user.location.split(",")),
-    success: onSuccess,
-    error: onFailure
+function getWeather(user) {
+  return new Promise((resolve, reject) => {
+    $.getJSON({
+      url: getLocationURL(user.location.split(",")),
+      success: resolve,
+      error: reject
+    });
   });
 }
 
 $("#btn").on("click", () => {
-  getUser(
-    "lastnamearya",
-    user => {
-      getWeather(
+  const userPromise = getUser("lastnamearya");
+
+  userPromise.then(user => {
+    const weatherPromise = getWeather(user);
+
+    weatherPromise.then(weather => {
+      updateUI({
         user,
-        weather => {
-          updateUI({
-            user,
-            weather: weather.query.results
-          });
-        },
-        showError
-      );
-    },
-    showError
-  );
+        weather: weather.query.results
+      });
+    });
+
+    weatherPromise.catch(showError);
+  });
+  userPromise.catch(showError);
 });
