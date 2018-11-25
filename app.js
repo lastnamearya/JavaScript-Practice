4eba165512e96d631be81095c6e88d44cb5bf381
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-25 16:39:02 +0530

    Callback async in sequential way

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-function add(x, y) {
-  return x + y;
+function showError(e) {
+  console.warn("Error", e);
 }
 
-function addFive(x, addReference) {
-  return addReference(5, x);
+function updateUI(info) {
+  $("#app").text(JSON.stringify(info));
 }
 
-console.log(addFive(10, add));
-
-// High-Order Function
-
-function higherOrderFunction(x, callback) {
-  return callback(5, x);
+function getLocationURL([city, state]) {
+  return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 }
 
-// With callbacks, we can delay the executon of a function
+const id = "lastnamearya";
 
-$("#btn").on("click", () => console.log("Callbacks are everywhere"));
+function getUser(id, onSuccess, onFailure) {
+  $.getJSON({
+    url: `https://api.github.com/users/${id}`,
+    success: onSuccess,
+    error: onFailure
+  });
+}
+
+function getWeather(user, onSuccess, onFailure) {
+  $.getJSON({
+    url: getLocationURL(user.location.split(",")),
+    success: onSuccess,
+    error: onFailure
+  });
+}
+
+$("#btn").on("click", () => {
+  getUser(
+    "lastnamearya",
+    user => {
+      getWeather(
+        user,
+        weather => {
+          updateUI({
+            user,
+            weather: weather.query.results
+          });
+        },
+        showError
+      );
+    },
+    showError
+  );
+});
