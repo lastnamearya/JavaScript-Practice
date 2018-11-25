9b68028d4750f3ec526a071628d87df7c2a68af1
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-25 15:57:23 +0530

    Higher Order Function

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
-function Animal(name, energy) {
-  this.name = name;
-  this.energy = energy;
+function add(x, y) {
+  return x + y;
 }
 
-Animal.prototype.eat = function(amount) {
-  console.log(`${this.name} is eating.`);
-  this.energy += amount;
-};
-
-Animal.prototype.sleep = function(length) {
-  console.log(`${this.name} is sleeping.`);
-  this.energy += length;
-};
-
-Animal.prototype.play = function(length) {
-  console.log(`${this.name} is playing.`);
-  this.energy -= length;
-};
-
-Animal.prototype.bark = function() {
-  console.log("Fuck You!!");
-};
-
-function Dog(name, energy, breed) {
-  Animal.call(this, name, energy);
-  this.breed = breed;
+function addFive(x, addReference) {
+  return addReference(5, x);
 }
 
-Dog.prototype = Object.create(Animal.prototype);
+console.log(addFive(10, add));
 
-Dog.prototype.bark = function() {
-  console.log("Woof Woof");
-  this.energy -= 0.1;
-};
+// High-Order Function
 
-// Reverting back to original constructor function that's changed during inheritance of the Parent Constructor function's prototype
-Dog.prototype.constructor = Dog;
+function higherOrderFunction(x, callback) {
+  return callback(5, x);
+}
 
-const charlie = new Dog("Charlie", 10, "Goldendoodle");
+// With callbacks, we can delay the executon of a function
+
+$("#btn").on("click", () => console.log("Callbacks are everywhere"));
