3c28c028c1a62d9e76fcec96181fe2b8217933c8
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-26 00:20:00 +0530

    Promise chain and catching error

--- app.js
+++ app.js
@@ -1,51 +1,29 @@
-function showError(e) {
-  console.warn("Error", e);
-}
-
-function updateUI(info) {
-  $("#app").text(JSON.stringify(info));
-}
-
-function getLocationURL([city, state]) {
-  return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
-}
-
-const id = "lastnamearya";
-
-function getUser(id) {
+function getPromise() {
   return new Promise((resolve, reject) => {
-    $.getJSON({
-      url: `https://api.github.com/users/${id}`,
-      success: resolve,
-      error: reject
-    });
+    setTimeout(resolve, 2000);
   });
 }
 
-function getWeather(user) {
-  return new Promise((resolve, reject) => {
-    $.getJSON({
-      url: getLocationURL(user.location.split(",")),
-      success: resolve,
-      error: reject
-    });
-  });
+function logA() {
+  console.log("A");
 }
 
-$("#btn").on("click", () => {
-  const userPromise = getUser("lastnamearya");
+function logB() {
+  console.log("B");
+}
 
-  userPromise.then(user => {
-    const weatherPromise = getWeather(user);
+function logCAndThrowError() {
+  console.log("C");
 
-    weatherPromise.then(weather => {
-      updateUI({
-        user,
-        weather: weather.query.results
-      });
-    });
+  throw new Error();
+}
 
-    weatherPromise.catch(showError);
-  });
-  userPromise.catch(showError);
-});
+function catchError() {
+  console.log("Error");
+}
+
+getPromise()
+  .then(logA)
+  .then(logB)
+  .then(logCAndThrowError)
+  .catch(catchError);
