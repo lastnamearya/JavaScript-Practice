382359941712d5b365fc1fd3cc44cee696f0fde1
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-26 23:36:11 +0530

    Promise Chaining

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
-function getPromise() {
+function showError(e) {
+  console.warn("Error", e);
+}
+
+function updateUI(info) {
+  $("#app").text(JSON.stringify(info));
+}
+
+function getLocationURL([city, state]) {
+  return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+}
+
+function getUser(id) {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000);
+    $.getJSON({
+      url: `https://api.github.com/users/${id}`,
+      success: resolve,
+      error: reject
+    });
   });
 }
 
-function logA() {
-  console.log("A");
+function getWeatherData(user) {
+  return new Promise((resolve, reject) => {
+    $.getJSON({
+      url: getLocationURL(user.location.split(",")),
+      success(weather) {
+        resolve({
+          user,
+          weather: weather.query.results
+        });
+      },
+      error: reject
+    });
+  });
 }
 
-function logB() {
-  console.log("B");
-}
-
-function logCAndThrowError() {
-  console.log("C");
-
-  throw new Error();
-}
-
-function catchError() {
-  console.log("Error");
-}
-
-getPromise()
-  .then(logA)
-  .then(logB)
-  .then(logCAndThrowError)
-  .catch(catchError);
+$("#btn").on("click", () => {
+  getUser("lastnamearya")
+    .then(user => getWeatherData(user))
+    .then(data => updateUI(data))
+    .catch(showError);
+});
