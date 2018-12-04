7d06d5f7a454c615af5c1941a0aaf9e1ffcf8767
lastnamearya <arya.jigyasu6815@gmail.com>
2018-12-04 23:33:16 +0530

    Looping with Generator Function

--- app.js
+++ app.js
@@ -36,3 +36,23 @@ function* generator() {
 }
 
 const i = generator();
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879 },
+  { first: 'Isaac', last: 'Newton', year: 1643 },
+  { first: 'Galileo', last: 'Galilei', year: 1564 },
+  { first: 'Marie', last: 'Curie', year: 1867 },
+  { first: 'Johannes', last: 'Kepler', year: 1571 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
+  { first: 'Max', last: 'Planck', year: 1858 }
+];
+
+function* loop(arr) {
+  // Only console.log for the first time
+  console.log(inventors);
+  for (const item of arr) {
+    yield item;
+  }
+}
+
+const inventorGen = loop(inventors);
