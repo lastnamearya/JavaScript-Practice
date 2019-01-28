4aa75f549e939757aab673ccede368da41886fe9
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-28 21:58:01 +0530

    Bind with Function Expression

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-function First(one, two) {
-  this.one = one;
-  this.two = two;
-}
+// Can Bind directly only with Function Expressions
 
-First.prototype.firstReturn = function() {
-  return console.log('Returned by First');
-};
-
-function Second(third, fourth) {
-  this.third = third;
-  this.fourth = fourth;
-}
-
-Second.prototype = Object.create(First.prototype);
-
-var x = new Second(3, 4);
+var sayHello = function(last_name) {
+  console.log('Hello ' + this + ' ' + last_name);
+}.bind('Asim');
+sayHello('Hussain');
