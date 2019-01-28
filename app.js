423cf1f001bc42a83548a5c5d2d206c94ab9eb70
lastnamearya <arya.jigyasu6815@gmail.com>
2019-01-28 18:02:35 +0530

    bind directly after expression

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Here this created in the interpetation pass.
 
-var a = function() {
-  console.log(this);
-};
-
 // That's how Bind solves our problem ~ binds this of the function expression to the global window object.
 
-a.bind(this);
+// We can directly bind this after the function expression.
+
+var a = function() {
+  console.log(this);
+}.bind(this);
 
 // Return undefined ~ rather than the global window object
 console.log(a());
